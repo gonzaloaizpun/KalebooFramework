4f27cd9d26b1c55a7a70e9938d43098272e50cfd
Smartium Developer <dev@smartiumtech.com>
2019-04-16 01:10:26 +0000

    getById and Listings

--- async/get.js
+++ async/get.js
@@ -30,92 +30,140 @@ var main = function(route, id, db_config, callback)
 	// ==============================================
 	connection.query(query, function(error, results) 
 	{
-		// Exit if there is not relationship
+		// Exit if there is not any kind of relationship
 		// ==============================================
-		var relationships = undefined;
-		var extensions = undefined;
-
-		if (relationships === undefined && extensions === undefined) {
+		if (!route.model.hasOne.length && !route.model.hasMany.length && !route.model.hasExternal.length) {
 			return end(callback, error, results);
+		}
+
+		if (!results.length) {
+			return end(callback, new KalebooError(`Mmm... it looks like ${id} does not exists`), null);
 		}
 
 		// Getting by id
 		if (isList) {
-			return listFunction(results, relationships, extensions, db_config, route, callback);
+			return listFunction(results, db_config, route, callback);
 		} else {
-			return classFunction(results[0], relationships, extensions, db_config, route, callback);
+			return classFunction(results[0], db_config, route, callback);
 		}
 
 	});
 }
 
-var listFunction = function(results, relationships, extensions, db_config, route, callback) 
-{
-    var functions = [];
-    for (var i = 0; i < results.length; i++) {
-        functions.push(Async.apply(classFunction, results[i], relationships, extensions, db_config, route));
-    }
+	var listFunction = function(results, db_config, route, callback) 
+	{
+	    var functions = [];
+	    for (var i = 0; i < results.length; i++) {
+	        functions.push(Async.apply(classFunction, results[i], db_config, route));
+	    }
 
-    Async.parallel(functions, function(error, results) {
-        end(callback, error, results);
-    });
-}
+	    Async.parallel(functions, function(error, results) {
+	        end(callback, error, results);
+	    });
+	}
 
-var classFunction = function(item, relationships, extensions, db_config, route, callback)
-{
-	// Init functions to be executed by Async
-	// ==============================================
-	var functions = [Async.apply(init, item)];
+	var classFunction = function(item, db_config, route, callback)
+	{
+		// Init functions to be executed by Async
+		// ==============================================
+		var functions = [Async.apply(init, item)];
 
-
-	// There is a few relationships... (ex. organization has a user)
-	// ==============================================
-	if (relationships !== undefined) {
-		for (var i = 0; i < relationships.length; i++) {
-			functions.push(relationshipFunction(relationships[i], db_config, item));
+		// hasOne relationship
+		// ==============================================
+		if (route.model.hasOne.length > 0) {
+			for (var i = 0; i < route.model.hasOne.length; i++) {
+				functions.push(hasOneFunction(route.model.hasOne[i], route, db_config, item));
+			}
 		}
+
+		// hasMany relationships
+		// ==============================================
+		if (route.model.hasMany.length > 0) {
+			for (var i = 0; i < route.model.hasMany.length; i++) {
+				functions.push(hasManyFunction(route.model.hasMany[i], route, db_config, item));
+			}
+		}
+
+		// hasExternal relationship
+		// ==============================================
+		if (route.model.hasExternal.length > 0) {
+			for (var i = 0; i < route.model.hasExternal.length; i++) {
+				functions.push(hasExternalFunction(route.model.hasExternal[i], route, db_config, item));
+			}
+		}
+
+	    Async.waterfall(functions, (error, result) => end(callback, error, result));
 	}
 
 
-	// There is a few extensions... (ex. organization has organization_attributes)
-	// ==============================================
-	if (extensions !== undefined) {
-		for (var i = 0; i < extensions.length; i++) {
-			functions.push(extensionFunction(extensions[i], route, db_config, item.id));
+		var hasOneFunction = function (relatedTable, route, db_config, item) 
+		{
+			let relationshipKey = 'id_' + relatedTable.slice(0, -1);
+			let attributeKey = relationshipKey.replace('id_' + route.model.table.slice(0, -1) + '_', '');
+			let idWhere = item[relationshipKey];
+			let keyWhere = 'id';
+
+			delete item[relationshipKey];
+
+			return Async.apply(query, db_config, keyWhere, idWhere, relatedTable, attributeKey, true);
 		}
-	}
 
-    Async.waterfall(functions, (error, result) => end(callback, error, result));
-}
+		var hasExternalFunction = function (relatedTable, route, db_config, item) 
+		{
+			let relationshipKey = 'id_' + relatedTable.slice(0, -1);
+			let attributeKey = relatedTable.slice(0, -1);
+			let idWhere = item[relationshipKey];
+			let keyWhere = 'id';
 
-var extensionFunction = function (extension, route, db_config, id) 
-{
-	let newModel = route.model.table.slice(0, -1) + '_' + extension;
-	let keyindex = 'id_' + route.model.table.slice(0, -1);
-	return Async.apply(asyncFunction, db_config, keyindex, id, newModel, extension);	
-}
+			delete item[relationshipKey];
 
-var relationshipFunction = function (relationship, db_config, item)
-{
-	let newModel 	= relationship;
-	let relation 	= 'id_' + newModel.slice(0, -1);
-	return Async.apply(asyncFunction, db_config, 'id', item[relation], newModel, relationship);
-}
+			return Async.apply(query, db_config, keyWhere, idWhere, relatedTable, attributeKey, true);
+		}
 
-var asyncFunction = function(db_config, keyindex, id, table, attributeKey, queue, callback)
-{
-	let connection 	= mysql.createConnection(db_config);
-	let query = `SELECT * FROM ${table} WHERE ${keyindex} = ${id}`;
+		var hasManyFunction = function (relatedTable, route, db_config, item)
+		{
+			let attributeKey = relatedTable.substring(relatedTable.indexOf("_") + 1);
+			let idWhere = item.id;
+			let keyWhere = 'id_' + relatedTable.substring(0, relatedTable.indexOf("_"));
 
-	connection.query(query, function(error, results) {
-		queue[attributeKey] = results;
-		callback(null, queue);
-	});
-}
+			return Async.apply(extendedQuery, db_config, keyWhere, idWhere, relatedTable, attributeKey, false);
+		}
+
+			var query = function(db_config, keyWhere, idWhere, table, attributeKey, onlyOneItem, queue, callback)
+			{
+				let connection 	= mysql.createConnection(db_config);
+				let query = `SELECT * FROM ${table} WHERE ${keyWhere} = ${idWhere}`;
+
+				connection.query(query, function(error, results) {
+					if (onlyOneItem) {
+						queue[attributeKey] = results[0];
+					} else {
+						queue[attributeKey] = results;
+					}
+					callback(null, queue);
+				});
+			}
+
+			var extendedQuery = function(db_config, keyWhere, idWhere, table, attributeKey, onlyOneItem, queue, callback)
+			{
+				let connection 	= mysql.createConnection(db_config);
+				let query = `SELECT * FROM ${table} WHERE ${keyWhere} = ${idWhere}`;
+
+				connection.query(query, function(error, results) {
+					if (onlyOneItem) {
+						queue[attributeKey] = results[0];
+					} else {
+						queue[attributeKey] = results;
+					}
+					callback(null, queue);
+				});
+			}
+
 
 var end = function(callback, error, results) {
     return callback(error, results);
 }
+
 
 var init = function (item, callback) {
 	callback(null, item);
