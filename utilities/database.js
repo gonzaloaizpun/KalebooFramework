8e999e56d1cd5f60c65e4ca16908d4b6850eecb4
Smartium Developer <dev@smartiumtech.com>
2019-10-11 03:20:11 +0000

    checking database config

--- utilities/database.js
+++ utilities/database.js
@@ -9,6 +9,14 @@ class Database
 	{
 		this.db_config = db_config;
 		connection = mysql.createConnection(this.db_config);
+		connection.connect();
+		connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+			if (error) {
+				console.log('ERROR: Cannot connect database.');
+				console.log('\nBye!');
+				process.exit(-1);
+			}
+		});
 	}
 	
 	query(query, fn) {
